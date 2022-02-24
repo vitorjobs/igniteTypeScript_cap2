a77b8beb30db9930b5d8f5a487555ddd4b8e784c
Vitor Guedes <vittorjobs@gmail.com>
2022-02-24 17:40:26 -0300

    Feat: Add UUID UUID para gerar identificar unico

--- 02_API_NodeJS/src/routes/categories.routes.ts
+++ 02_API_NodeJS/src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+import {v4 as uuidV4} from 'uuid'
 
 const categoriesRoutes = Router()
 
@@ -7,12 +8,14 @@ const categoriesRoutes = Router()
 	categoriesRoutes.post("/categories", (request, response) =>{
 		const {name, description} = request.body
 
-		categories.push({
+		const category ={
 			name, 
-			description
-		})
+			description,
+			id: uuidV4()
+		}
+		categories.push(category)
 
 		return response.status(201).send()
 	})	
 
-export {categoriesRoutes}
+export {categoriesRoutes} 
