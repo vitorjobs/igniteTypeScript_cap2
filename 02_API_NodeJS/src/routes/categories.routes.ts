0c4a86152c585388a62ddfed4a9da09693087d1d
Vitor Guedes <vittorjobs@gmail.com>
2022-03-16 09:21:29 -0300

    Feat: Estruturaçao do Model Category e Reestruturaçao de categoriesRoutes

--- 02_API_NodeJS/src/routes/categories.routes.ts
+++ 02_API_NodeJS/src/routes/categories.routes.ts
@@ -1,16 +1,18 @@
 import {Router} from 'express'
 import {v4 as uuidV4} from 'uuid'
+import { Category } from '../model/Category'
 
 const categoriesRoutes = Router()
 
-	const categories = []
+	const categories: Category[] = []
 
 	categoriesRoutes.post("/", (request, response) =>{
 		const {name, description} = request.body
 
-		const category ={
+		const category: Category ={
 			name, 
 			description,
+			created_at: new Date(),
 			id: uuidV4()
 		}
 		categories.push(category)
