afb0b80b865b2f79e5313a9f6cf0074cd7ab3dcc
Vitor Guedes <vittorjobs@gmail.com>
2022-03-16 10:47:22 -0300

    Feat: Reestruturaçao do arquivo de rotas

--- 02_API_NodeJS/src/routes/categories.routes.ts
+++ 02_API_NodeJS/src/routes/categories.routes.ts
@@ -1,27 +1,15 @@
 import {Router} from 'express'
-import {v4 as uuidV4} from 'uuid'
-import { Category } from '../model/Category'
+
+import {CategoriesRepository } from '../repositories/CategoriesRepository'
 
 const categoriesRoutes = Router()
 
-	const categories: Category[] = []
+const categoriesRepository = new CategoriesRepository()
 
-	categoriesRoutes.post("/", (request, response) =>{
-		const {name, description} = request.body
+categoriesRoutes.post("/", (request, response) => {
+  const {name, description} = request.body
 
-		const category = new Category ()
-
-    Object.assign(category, {
-      name, 
-			description,
-			created_at: new Date(),
-    })
-	
-		  categories.push(category)
-
-		return response.status(201).json({
-      category
-    })
-	})	
+  return response.status(201).send()
+})
 
 export {categoriesRoutes} 
