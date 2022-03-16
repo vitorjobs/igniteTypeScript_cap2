ae22ce05b96aa78574dd21289980ec596ecc6026
Vitor Guedes <vittorjobs@gmail.com>
2022-03-16 10:45:36 -0300

    Feat: Estruturaçao do endpoint para criar uma categoria

---
+++ 02_API_NodeJS/src/repositories/CategoriesRepository.ts
@@ -0,0 +1,31 @@
+import { Category } from "../model/Category"
+
+// QUANDO O REPOSITÓRIO FOI INICIADO O `CATEGORIES` SERÁ INICIADO TAMBÉM
+
+interface ICreateCategoryDTO{
+  name: string,
+  description: string
+}
+
+class CategoriesRepository {
+  private categories: Category []
+
+  constructor(){
+    this.categories = []
+  }
+
+  // CADASTRO UMA CATEGORIA
+  create({name, description}: ICreateCategoryDTO): void {
+    const category = new Category ()
+
+    Object.assign(category, {
+      name, 
+			description,
+			created_at: new Date(),
+    })
+	
+		this.categories.push(category)
+  }
+}
+
+export {CategoriesRepository}
