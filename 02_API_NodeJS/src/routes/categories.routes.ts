82ded422ec0608f2469881a931d24fcd47acc074
Vitor Guedes <vittorjobs@gmail.com>
2022-03-16 12:05:57 -0300

    Feat: Estruturaçao do endpoint para criar uma categoria e listar

--- 02_API_NodeJS/src/routes/categories.routes.ts
+++ 02_API_NodeJS/src/routes/categories.routes.ts
@@ -9,7 +9,16 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post("/", (request, response) => {
   const {name, description} = request.body
 
+  categoriesRepository.create({
+    name, description
+  })
   return response.status(201).send()
+})
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list()
+
+  return response.json(all)
 })
 
 export {categoriesRoutes} 
