9800b48227dacf77feda8333275f7d89fbdb171a
Vitor Guedes <vittorjobs@gmail.com>
2022-03-23 15:23:06 -0300

    Feat: Controller e UserCase - List

---
+++ 02_API_NodeJS/src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,19 @@
+import { Category } from "../../model/Category"
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository"
+
+interface IRequest {
+  name: string
+  description: string
+}
+
+class ListCategoriesUseCase {
+
+  constructor(private categoriesRepository: ICategoriesRepository){}
+  
+  execute(): Category [] {
+    const categories = this.categoriesRepository.list()
+    return categories
+  }
+}
+
+export {ListCategoriesUseCase}
